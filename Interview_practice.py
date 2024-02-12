194d71defddc9fb7ff5f29807bda09bc562216af
Durga Bhattarai <durga@Durgas-MacBook-Air.local>
2024-02-12 09:32:02 +0545

    edited

--- Interview_practice.py
+++
@@ -1,4 +0,0 @@
-# Write a Python function that takes two lists of integers as
-# input and returns a new list containing only the numbers that
-# are present in both lists,
-# but with each number appearing only once in the final list.
